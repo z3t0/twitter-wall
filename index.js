5e236b29eafff2101cc8ef27ae39bca425ffc8c9
Rafi Khan <zetoslab@gmail.com>
2016-09-06 21:04:58 -0600

    added time module

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var tweets = [];
 //as
-var address = "http://192.168.0.215:3000"
+var address = "localhost:3000"
 var socket = require('socket.io-client')(address);
 socket.on('connect', function(){});
 socket.on('event', function(data){
@@ -10,44 +10,8 @@ socket.on('disconnect', function(){
 socket.on('tweet', function(data) {
     // Got a tweet from server
     // add to the list
-    updateStream(data)
+    tweets.push(data)
 })
-function updateStream(tweetData) {
-    var node = document.getElementById('stream')
-
-    // Create Stream Object
-
-    var item = document.createElement('li')
-    item.className = 'streamItem'
-
-    var user = document.createElement('div')
-    var user_fullname = document.createElement('div')
-    var user_image = document.createElement("IMG")
-
-    user.className = 'user'
-    user_fullname.className = 'user_fullname'
-    user_image.className = 'user_image'
-    user.appendChild(user_image)
-    user.appendChild(user_fullname)
-    
-
-    var tweet = document.createElement('div')
-    var tweet_text = parseText(tweetData.tweet_text)
-
-    tweet.className = 'tweet'
-    tweet_text.className = 'tweet_text'
-    tweet.appendChild(tweet_text)
-
-    user_fullname.appendChild(document.createTextNode(tweetData.user_fullname))
-    user_image.src = tweetData.user_image_src
-
-    item.appendChild(user)
-    item.appendChild(tweet)
-
-    tweets.push(tweetData)
-
-    node.appendChild(item)
-}
 
 function setCurrentTweet(tweetData) {
     var node = document.getElementById('current')
@@ -75,10 +39,10 @@ function setCurrentTweet(tweetData) {
     var user_name_screen = document.createElement('div')
     var user_image = document.createElement("IMG")
 
-    user_name.className = 'current_user_name' 
+    user_name.className = 'current_user_name'
     user_name_full.className = 'current_user_name_full'
     user_name_screen.className = 'current_user_name_screen'
-    
+
     user_image.className = 'current_user_image'
 
     user_name.appendChild(user_name_full)
@@ -87,13 +51,35 @@ function setCurrentTweet(tweetData) {
     user_name_full.appendChild(document.createTextNode(tweetData.user_fullname))
     user_name_screen.appendChild(document.createTextNode(tweetData.user_name))
     user_image.src = tweetData.user_image_src
-    
+
     // Tweet
     var tweet = document.createElement('div')
 
     tweet.className = 'current_tweet'
     tweet.appendChild(document.createTextNode(tweetData.tweet_text))
 
+    // Time
+    var time = document.createElement('div')
+    time.className = "current_time"
+
+    // Date Object needs to be created locally
+    var date = new Date(
+        tweetData.date.replace(/^\w+ (\w+) (\d+) ([\d:]+) \+0000 (\d+)$/,
+            "$1 $2 $4 $3 UTC"));
+
+    var dateTime = document.createElement('div')
+    var localTime= document.createElement('div')
+
+    time.appendChild(dateTime)
+    time.appendChild(localTime)
+
+    dateTime.className = "current_date_time"
+    localTime.className = "current_local_time"
+
+    localTime.appendChild(document.createTextNode(date.toLocaleTimeString()))
+    dateTime.appendChild(document.createTextNode(date.toLocaleDateString()))
+
+    // Media
     if (tweetData.has_media)
     {
         for (var i = 0; i < tweetData.media.length; i++) {
@@ -108,6 +94,7 @@ function setCurrentTweet(tweetData) {
 
     item.appendChild(user_name)
     item.appendChild(user_image)
+    item.appendChild(time)
     item.appendChild(tweet)
 
     node.appendChild(item)
@@ -122,6 +109,7 @@ function parseText(text) {
     for (var i = 0; i < data.length; i++) {
         var word = data[i]
 
+
         if (i < (data.length - 1)) {
         }
 
@@ -130,24 +118,34 @@ function parseText(text) {
 
         // Mention
         if (word.includes("@")) {
-            element.className = "mention"
+            string = string + '<span style="color:yellow">' + word +'</span>' + " "
         }
 
         // tag
         else if (word.includes("#")) {
-            element.className = "tag"
+            string = string + '<span style="color:blue">' + word +'</span>' + " "
         }
 
         else if (i == (data.length - 1)) {
-            element.className = "last"
+            string = string + word
+        }
+
+        else if (word.includes("http")) {
+            // we dont want any links
+            return
         }
 
         else {
-            element.className = "word"
+            string = string + word + " "
         }
 
-        node.appendChild(element)
     }
+
+    var e = document.createElement('div')
+    e.innerHTML = string
+    console.log(string)
+
+    node.appendChild(e)
 
     console.log(node)
     return node
